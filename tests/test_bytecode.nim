3ad4dc9e27c71d1c49522112ed583ea015a4b67b
Tony Solomonik <tony.solomonik@gmail.com>
2021-02-27 16:44:37 +0200

    tests: Add bytecode builder sanity test

---
+++ tests/test_bytecode.nim
@@ -0,0 +1,32 @@
+import unittest
+import options
+
+import frontend/frontend
+import ir/[bytecodebuilder, opcodes, variables]
+
+suite "bytecode builder":
+    test "sanity":
+        let outputAst = "let a = 5 + 9 * 2\n".parseText()
+        assert outputAst.isSome()
+        let b = outputAst.get().byteCode()
+        assert b.consts.len() == 3
+        assert b.opcodes.len() == 6
+
+        for i, expectedConstValue in @["5", "9", "2"]:
+            assert b.consts[i].value == expectedConstValue
+
+        assert b.opcodes[0].kind == LoadConst
+        assert b.opcodes[0].value == 0
+
+        assert b.opcodes[1].kind == LoadConst
+        assert b.opcodes[1].value == 1
+
+        assert b.opcodes[2].kind == LoadConst
+        assert b.opcodes[2].value == 2
+
+        assert b.opcodes[3].kind == Multiply
+
+        assert b.opcodes[4].kind == Add
+
+        assert b.opcodes[5].kind == StoreVar
+        assert b.opcodes[5].value == 0
