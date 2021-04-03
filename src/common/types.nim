18f23fe4582ceb1a3b8221578ff8e0ced93abe5e
Tony Solomonik <tony.solomonik@gmail.com>
2021-04-03 16:30:35 +0300

    types: Add more integer types
    Previously we only had s32, u32, and f32.
    
    This commit adds s8, s16, s64, u8, u16, u64, f16, and f64.

--- src/common/types.nim
+++ src/common/types.nim
@@ -13,11 +13,23 @@ type
         Void
         Procedure
 
-        Signed32
-        Unsigned32
-        Float32
         Boolean
         String
+
+        # When adding integer types, remember to update the is*Integer functions
+        Signed8
+        Signed16
+        Signed32
+        Signed64
+
+        Unsigned8
+        Unsigned16
+        Unsigned32
+        Unsigned64
+
+        Float16
+        Float32
+        Float64
 
     Type* = ref object
         ptrLevel*: int  # type* == 1, type*** == 3
@@ -36,17 +48,54 @@ const BOOLEAN_FALSE* = "false"
 const BUILTIN_TYPES* = {
     "auto": Auto,
     "void": Void,
-    "s32": Signed32,
-    "u32": Unsigned32,
-    "f32": Float32,
+
     "bool": Boolean,
     "string": String,
+
+    "s8": Signed8,
+    "s16": Signed16,
+    "s32": Signed32,
+    "s64": Signed64,
+
+    "u8": Unsigned8,
+    "u16": Unsigned16,
+    "u32": Unsigned32,
+    "u64": Unsigned64,
+
+    "f16": Float16,
+    "f32": Float32,
+    "f64": Float64,
 }
 
-proc defaultValue*(t: Type): Option[string] =
+proc isSignedInteger*(t: Type): bool =
     case t.kind:
-    of Signed32, Unsigned32, Float32:
-        some[string]("0")
+    of Signed8, Signed16, Signed32, Signed64:
+        true
+    else:
+        false
+
+proc isUnsignedInteger*(t: Type): bool =
+    case t.kind:
+    of Unsigned8, Unsigned16, Unsigned32, Unsigned64:
+        true
+    else:
+        false
+
+proc isRealInteger*(t: Type): bool =
+    case t.kind:
+    of Float16, Float32, Float64:
+        true
+    else:
+        false
+
+proc isInteger*(t: Type): bool =
+    isSignedInteger(t) or isUnsignedInteger(t) or isRealInteger(t)
+
+proc defaultValue*(t: Type): Option[string] =
+    if t.isInteger():
+        return some[string]("0")
+
+    case t.kind:
     of String:
         some[string]("")
     of Boolean:
