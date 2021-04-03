88d1e093caa77f85cd3e3c3fddd680319cdbc1dd
Tony Solomonik <tony.solomonik@gmail.com>
2021-04-03 19:31:30 +0300

    ir: bytecode: Prettier string printing
    Strings are now printed with quotes.
    
    If a string is too big, slice it and put ellipsis (...) after.

--- src/ir/variables.nim
+++ src/ir/variables.nim
@@ -3,13 +3,26 @@ import strformat
 import frontend/ast
 import common/types
 
+const MAX_VALUE_DISPLAY_LENGTH = 20
+
 type
     Variable* = ref object
         typ*: Type  # `type` is a saved keyword in nim
         value*: string
 
 func pretty*(v: Variable, showType: bool = true): string =
-    result = v.value
+    result = ""
+
+    let value = if v.value.len() > MAX_VALUE_DISPLAY_LENGTH:
+        v.value[0..<MAX_VALUE_DISPLAY_LENGTH] & "..."
+    else:
+        v.value
+
+    if v.typ.kind == String:
+        result.addQuoted(value)
+    else:
+        result = value
+
     if showType:
         result &= &" ({v.typ})"
 
